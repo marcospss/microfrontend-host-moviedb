8835d18487c09c0b835b5124f40007cf0d71597c
marcospss <mps@mps.com>
2019-02-12 21:09:06 -0200

    Home: Inclusão dos componentes

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,17 +1,22 @@
 import React, { Component } from "react";
+import axios from 'axios';
+
 import { DiscoverProvider, CommonProvider } from './../core/services/index';
+import { LoadingAnimation, CardBackdropImage, CardPosterImage } from './../core/components/index';
 class Home extends Component {
-    
-   
+    state = {
+        popular: [],
+        topRated: [],
+        isLoading: true,
+        errors: null
+    }
 
     constructor() {
         super();
         this.discover = new DiscoverProvider();
         this.common = new CommonProvider();
     }
-
     
-
     componentDidMount() {
         this.filterDiscoverProperties = {
             mediaType: 'movie',
@@ -19,46 +24,100 @@ class Home extends Component {
             year: '',
             genre: ''
         };
-        this.discover.getDiscover(this.filterDiscoverProperties).then(response => {
-            console.log('DISCOVER -> ', response.data);
-        });
+        const filterTopRatedProperties = {
+            mediaType: 'movie'
+        };
+        axios.all([this.discover.getDiscover(this.filterDiscoverProperties), this.common.getTopRated(filterTopRatedProperties)])
+            .then(axios.spread((popular, topRated) => {
+                this.setState({
+                    popular: popular.data.results,
+                    topRated: topRated.data.results,
+                    isLoading: false
+                  });
+            }))
+            .catch(error => this.setState({ error, isLoading: false }));
     }
       
   render() {
+    const { popular, topRated, isLoading } = this.state;
     return (
-      <div className="page">
-            <article className="row">
-                <header className="col-sm-12">
-                    <h1 className="section-title">Popular Movies</h1>
-                </header>
-                <div className="col-md-9">
-                    <div className="slider">
-                        <h1>Carrossel</h1>
-                    </div>
-                </div>
-                <div className="col-md-3">
-                    <div className="row">
-                        <div className="col-sm-6 col-md-12">
+        <>
+        {!isLoading ? (
+            <div className="page">
+                <article className="row">
+                    <header className="col-sm-12">
+                        <h1 className="section-title">Popular Movies</h1>
+                    </header>
+                    <div className="col-md-9">
+                        <div className="slider">
+                            <h1>Carrossel</h1>
                         </div>
                     </div>
-                </div>
-            </article>
-            <article className="row">
-                <div className="col-sm-6 col-md-3">
-                </div>
-            </article>
-            <article className="row">
-                <header className="col-sm-12">
-                    <h1 className="section-title">Top Rated Movies</h1>
-                </header>
-                <div className="col-sm-12">
-                    <ul className="list-unstyled">
-                        <li className="col-sm-12 col-md-4">
-                        </li>
-                    </ul>
-                </div>
-            </article>
-      </div>
+                    <div className="col-md-3">
+                        <div className="row">
+                            { 
+                                popular.splice(4,3).map(item => {
+                                    const { id } = item;
+                                    return (
+                                        <div key={id} className="col-sm-6 col-md-12">
+                                            <CardBackdropImage
+                                                data={item}
+                                                styleName="latest-movie"
+                                                mediaType="movie"
+                                                showOverview={false}
+                                            />
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                    </div>
+                </article>
+                <article className="row">
+                    { 
+                        popular.splice(4,4).map(item => {
+                            const { id } = item;
+                            return (
+                                <div key={id} className="col-sm-6 col-md-3">
+                                    <CardBackdropImage
+                                        data={item}
+                                        styleName="latest-movie"
+                                        mediaType="movie"
+                                        showOverview={true}
+                                    />
+                                </div>
+                            )
+                        })
+                    }
+                </article>
+                <article className="row">
+                    <header className="col-sm-12">
+                        <h1 className="section-title">Top Rated Movies</h1>
+                    </header>
+                    <div className="col-sm-12">
+                        <ul className="list-unstyled">
+                            { 
+                                topRated.splice(0,9).map(item => {
+                                    const { id } = item;
+                                    return (
+                                        <li key={id} className="col-sm-12 col-md-4">
+                                            <CardPosterImage 
+                                                data={item}
+                                                mediaType="movie"
+                                            />
+                                        </li>
+                                    )
+                                })
+                            }
+                        </ul>
+                    </div>
+                </article>
+            </div>
+          ) : (
+            <LoadingAnimation />
+          )
+        }
+        </>
     );
   }
 }
