14623dd7f21278e6ee65e16187045f22ebadf5f1
Marcos Paulo <930447+marcospss@users.noreply.github.com>
2019-02-19 17:31:00 -0300

    Adicionando LocalStorage

--- src/core/services/index.js
+++ src/core/services/index.js
@@ -2,3 +2,4 @@ export { DiscoverProvider } from './DiscoverProvider';
 export { SearchProvider } from './SearchProvider';
 export { CommonProvider } from './CommonProvider';
 export { HelperProvider } from './HelperProvider';
+export { LocalStorage } from './LocalStorage';
