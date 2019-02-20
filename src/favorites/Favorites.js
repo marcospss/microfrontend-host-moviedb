5a59b4d86b48b7bb9cb24c86bc8c7aa2a17dfab7
Marcos Paulo <930447+marcospss@users.noreply.github.com>
2019-02-20 11:14:02 -0300

    Componente - Favoritos: Listagem dos favoritos

--- src/favorites/Favorites.js
+++ src/favorites/Favorites.js
@@ -1,27 +1,71 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { HelperProvider, LocalStorage } from './../core/services/index';
+import { LoadingAnimation } from './../core/components/index';
 class Favorites extends Component {
+
+  state = {
+    collection: [],
+    isLoading: true
+}
+
+constructor(props) {
+    super(props);
+    this.helper = new HelperProvider();
+    this.store = new LocalStorage();
+}
+
+componentDidMount() {
+  this.store.getAll().then(data => {
+      if(!!data.length) {
+          this.setState({
+              collection: data
+          });
+      }
+      this.setState({
+          isLoading: false
+      });
+  });
+}
   render() {
+    const { isLoading, collection } = this.state;
     return (
-      <div className="page">
-        <div className="breadcrumbs">
-          <Link to='/'>Home</Link>
-          <span>Favorites</span>
-        </div>
-        <div className="row">
-
-          <article className="col-md-3">
-          <div className="team">
-          <figure className="team-image">
-
-          </figure>
-          <h2 className="team-name">ddddd</h2>
-          <small className="team-title">dddd</small>
-          </div>
-          </article>
-
-        </div>
-      </div>
+      <>
+        { !isLoading ? (
+            <div className="page">
+              <div className="breadcrumbs">
+                <Link to='/'>Home</Link>
+                <span>Favorites</span>
+              </div>
+              <div className="row">
+                {
+                  collection.map(item => {
+                    const { id, mediaType, poster_path, overview } = item;
+                    let linkMedia = `/details/${mediaType}/${id}`;
+                    return (
+                      <article key={id} className="col-md-3">
+                        <div  className="team">
+                          <figure className="team-image">
+                          <Link to={ linkMedia }>
+                              <img src={ this.helper.posterImage(poster_path, 'w185') } alt={ this.helper.title(item) } />
+                          </Link>
+                          </figure>
+                          <h2 className="team-name">
+                            <Link to={ linkMedia }>{ this.helper.title(item) }</Link>
+                          </h2>
+                          <small className="team-title">{ overview.substring(0, 160) }...</small>
+                        </div>
+                      </article>
+                    )
+                  })
+                }
+              </div>
+            </div>
+            ) : (
+              <LoadingAnimation />
+            )
+        }
+    </>
     );
   }
 }
