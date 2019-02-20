ba0983f70e681d41f5fbe09eea789a1b29ea769e
Marcos Paulo <930447+marcospss@users.noreply.github.com>
2019-02-20 17:03:10 -0300

    Componente Search: Implementação

--- src/core/components/Search.js
+++ src/core/components/Search.js
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
+import { HelperProvider, SearchProvider } from './../services/index';
+class Search extends Component {
+    state = {
+        collection: [],
+        showInputSearch: false
+    }
 
-const Search = () => {
-    return (
-        <form class="search-form" role="search">
-            <input type="text" name="query" id="query" placeholder="Search..." />
-            <button><i class="fa fa-search"></i></button>
-        </form>
-    );
+    constructor() {
+        super();
+        this.helper = new HelperProvider();
+        this.search = new SearchProvider();
+    }
+
+    searchMedia(query) {
+        this.search.getMultiSearch(query)
+            .then(result => {
+                this.setState({
+                    collection: result.data.results
+                });
+                console.log('getMultiSearch -> ', result.data);
+            })
+            .catch(error => this.setState({ error, isLoading: false }));
+    };
+    
+    showInputSearch(e) {
+        e.preventDefault();
+        this.setState({
+            showInputSearch: !this.state.showInputSearch
+        });
+    };
+    render() {
+        const { showInputSearch } = this.state;
+        return (
+            <form className="search-form" role="search">
+                <input onChange={ (e) => this.searchMedia(e.currentTarget.value) } type="text" name="query" id="query" placeholder="Search..." maxLength="30" className={ showInputSearch ? 'active' : '' } />
+                <button onClick={ (e) => this.showInputSearch(e) }><i className="fa fa-search"></i></button>
+            </form>
+        );
+    }
 };
 
 export default Search;
