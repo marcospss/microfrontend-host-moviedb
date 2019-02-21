e79eba1c3de3ebe2baccecbcd5d858779deaa4f6
Marcos Paulo <930447+marcospss@users.noreply.github.com>
2019-02-21 15:22:04 -0300

    Service - HelperProvider
    Correção caminho da imagem padrão

--- src/core/services/HelperProvider.js
+++ src/core/services/HelperProvider.js
@@ -1,5 +1,7 @@
 import { SETTINGS as IMAGE_SETTINGS } from "../config/ImageSettings";
 import axios from 'axios';
+import defaultImage from './../../assets/images/default-image.png';
+
 export class HelperProvider {
 
     /**
@@ -21,7 +23,7 @@ export class HelperProvider {
     posterImage(poster_path, size = 'w92') {
         const sizeImage = IMAGE_SETTINGS.poster_sizes[size];
         if (!poster_path) {
-            return 'assets/images/default-image.png';
+            return `${defaultImage}`;
         }
         return `${IMAGE_SETTINGS.secure_base_url}${sizeImage}${poster_path}`;
     }
@@ -29,7 +31,7 @@ export class HelperProvider {
     backdropImage(backdrop_path, size = 'w300') {
         const sizeImage = IMAGE_SETTINGS.backdrop_sizes[size];
         if (!backdrop_path) {
-            return 'assets/images/default-image.png';
+            return `${defaultImage}`;
         }
         return `${IMAGE_SETTINGS.secure_base_url}${sizeImage}${backdrop_path}`;
     }
@@ -37,7 +39,7 @@ export class HelperProvider {
     profileImage(profile_sizes, size = 'w185') {
         const sizeImage = IMAGE_SETTINGS.profile_sizes[size];
         if (!profile_sizes) {
-            return 'assets/images/default-image.png';
+            return `${defaultImage}`;
         }
         return `${IMAGE_SETTINGS.secure_base_url}${sizeImage}${profile_sizes}`;
     }
